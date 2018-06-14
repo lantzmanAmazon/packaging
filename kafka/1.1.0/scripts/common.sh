dfb48a3ba0d0044f5378556b9ea56cba65622495
Lantzman <lantzman@amazon.com>
2018-06-14 17:57:08 +0100

    Update scripts to use local file

---
+++ kafka/1.1.0/scripts/common.sh
@@ -0,0 +1,3 @@
+KafkaDownloadURL=http://ftp.heanet.ie/mirrors/www.apache.org/dist/kafka/1.1.0/kafka_2.12-1.1.0.tgz
+KafkaInstallDirectory=1.1.0
+KafkaFile=kafka_2.12-1.1.0.tgz
--- kafka/1.1.0/scripts/install_dependencies.sh
+++ kafka/1.1.0/scripts/install_dependencies.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+source common.sh
 
 echo Installing kafka $KafkaDownloadURL...
-export PATH=~/bin:$PATH
-export ZOOKEEPER_ID=1
-export file=`basename ${KafkaDownloadURL}`
+file=`basename ${KafkaDownloadURL}`
+export KAFKA_HOME=/app/kafka/$KafkaInstallDirectory
+export PATH=~/bin:$PATH:$KAFKA_HOME/bin
 
+echo Downloading Kafka to $KAFKA_HOME ...
 su - root -c 'mkdir -p /app/kafka'
-wget $KafkaDownloadURL -P /app
+cp ../$KafkaFile /app/$KafkaFile
+tar -zxvf /app/$KafkaFile -C /app/kafka
 
-tar -zxvf /app/$file -C /app/kafka
+echo Generating zookeeper id ..
 su - root -c 'mkdir -p /tmp/zookeeper'
-python /tmp/zookeeperIdGenerator.py
-echo "initLimit=5" >> /app/kafka/$KafkaInstallDirectory/config/zookeeper.properties
-echo "syncLimit=2" >> /app/kafka/$KafkaInstallDirectory/config/zookeeper.properties
+python zookeeperIdGenerator.py
+source /tmp/zookeeper/myid
+export ZOOKEEPER_ID=$ZOOKEEPERID
+
+echo Applying Zookeeper config
+echo "initLimit=5" >> $KAFKA_HOME/config/zookeeper.properties
+echo "syncLimit=2" >> $KAFKA_HOME/config/zookeeper.properties
 
 sed -i 's/Defaults    requiretty/Defaults    !requiretty/g' /etc/sudoers
