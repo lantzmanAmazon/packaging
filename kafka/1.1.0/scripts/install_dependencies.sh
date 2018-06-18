72e6d66d8afd7f9ebff124938179a9ba91576085
Lantzman <lantzman@amazon.com>
2018-06-18 18:06:42 +0100

    debug

--- kafka/1.1.0/scripts/install_dependencies.sh
+++ kafka/1.1.0/scripts/install_dependencies.sh
@@ -20,7 +20,10 @@ source /tmp/zookeeper/myid
 export ZOOKEEPER_ID=$ZOOKEEPERID
 
 echo Applying Zookeeper config `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties
+
+set -x	
 su - root -c "cp `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties $KAFKA_HOME/config/zookeeper.properties"
+set +x	
 echo "$(cat $KAFKA_HOME/config/zookeeper.properties)"
 
 sed -i 's/Defaults    requiretty/Defaults    !requiretty/g' /etc/sudoers
