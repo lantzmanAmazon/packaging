d744c993567cfb75bc6ff966756bf283065dc444
Lantzman <lantzman@amazon.com>
2018-06-19 14:20:25 +0100

    Move variables to start

--- kafka/1.1.0/scripts/common.sh
+++ kafka/1.1.0/scripts/common.sh
@@ -5,10 +5,6 @@ ZKSetupNode=quorum # TODO: make an argument
 
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
-PRIVATE_IP=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep privateIp | awk -F\" '{print $4}')
-
-# The main assumption is that the IP determins the ID, and we control the IPs even when chaning instances
-SERVER_ID=$(cat config/zookeeper.properties | grep $PRIVATE_IP | awk -F"=" '{print $1}' | awk -F"." '{print $2}')
 
 export ZK_CONFIG_TYPE=$ZKSetup
 export ZK_NODE_TYPE=$ZKSetupNode
