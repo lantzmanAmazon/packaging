7f1d561be5350cc9167f27117147a19faf997502
Lantzman <lantzman@amazon.com>
2018-06-19 12:59:27 +0100

    Redirect err to out

--- kafka/1.1.0/scripts/stop_server.sh
+++ kafka/1.1.0/scripts/stop_server.sh
@@ -2,4 +2,6 @@
 echo Stopping ...
 source `dirname "$0"`/common.sh
 
-$KAFKA_HOME/bin/zookeeper-server-stop.sh
+# Stop, redirect failures to output to avoid not being
+# able to deploy if the instance failed to start initially
+$KAFKA_HOME/bin/zookeeper-server-stop.sh 2>&1 
