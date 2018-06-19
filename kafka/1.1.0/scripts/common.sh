247b9d6b893cabaff28cd0b5996c8030778947c9
Lantzman <lantzman@amazon.com>
2018-06-19 12:51:18 +0100

    Change default config

--- kafka/1.1.0/scripts/common.sh
+++ kafka/1.1.0/scripts/common.sh
@@ -3,6 +3,9 @@ KafkaInstallDirectory=`(basename $KafkaFile .tgz)`
 ZKSetup=5nodesetup # TODO: make an argument
 ZKSetupNode=quorum # TODO: make an argument
 
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+
 export ZK_CONFIG_TYPE=$ZKSetup
 export ZK_NODE_TYPE=$ZKSetupNode
 export KAFKA_HOME=/app/kafka/$KafkaInstallDirectory
--- kafka/1.1.0/scripts/install_dependencies.sh
+++ kafka/1.1.0/scripts/install_dependencies.sh
@@ -8,14 +8,13 @@ su - root -c 'mkdir -p /app/kafka'
 su - root -c  "cp `dirname "$0"`/../$KafkaFile /app/$KafkaFile"
 tar -zxvf /app/$KafkaFile -C /app/kafka
 
-echo Generating zookeeper id ..
-su - root -c 'mkdir -p /tmp/zookeeper'
-python `dirname "$0"`/zookeeperIdGenerator.py
-source /tmp/zookeeper/myid
-export ZOOKEEPER_ID=$ZOOKEEPERID
+#echo Generating zookeeper id ..
+#su - root -c 'mkdir -p /tmp/zookeeper'
+#python `dirname "$0"`/zookeeperIdGenerator.py
+#source /tmp/zookeeper/myid
+#export ZOOKEEPER_ID=$ZOOKEEPERID
 
 echo Applying Zookeeper config `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties
-
 su - root -c "cp `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties $KAFKA_HOME/config/zookeeper.properties"
 echo "$(cat $KAFKA_HOME/config/zookeeper.properties)"
 
