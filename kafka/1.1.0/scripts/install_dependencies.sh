011f53464f594c409aa1dde77737c3e235c7292b
Lantzman <lantzman@amazon.com>
2018-06-19 14:31:40 +0100

    Fix script

--- kafka/1.1.0/scripts/install_dependencies.sh
+++ kafka/1.1.0/scripts/install_dependencies.sh
@@ -8,6 +8,10 @@ su - root -c 'mkdir -p /app/kafka'
 su - root -c  "cp `dirname "$0"`/../$KafkaFile /app/$KafkaFile"
 tar -zxvf /app/$KafkaFile -C /app/kafka
 
+echo Applying Zookeeper config `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties
+su - root -c "cp `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties $KAFKA_HOME/config/zookeeper.properties"
+echo "$(cat $KAFKA_HOME/config/zookeeper.properties)"
+
 PRIVATE_IP=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep privateIp | awk -F\" '{print $4}')
 echo Private IP: $PRIVATE_IP
 # The main assumption is that the IP determins the ID, and we control the IPs even when chaning instances
@@ -17,13 +21,7 @@ if [ -z "$SERVER_ID" ]; then
     echo Could not find server Id
     exit 1
 fi
-#TODO: add error handling
-
-#TODO: data should be defined in a separate volume
+#TODO: data should be defined in a separate volume (also need to update the properties to reflect that)
 echo Generating zookeeper myid $SERVER_ID file in $KAFKA_HOME/data/myid   ..
 su - root -c "mkdir -p $KAFKA_HOME/data"
 su - root -c "echo $SERVER_ID > $KAFKA_HOME/data/myid"
-
-echo Applying Zookeeper config `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties
-su - root -c "cp `dirname "$0"`/$ZK_CONFIG_TYPE/$ZK_NODE_TYPE.properties $KAFKA_HOME/config/zookeeper.properties"
-echo "$(cat $KAFKA_HOME/config/zookeeper.properties)"
