11551823a90340d181e02e8e86d7ef0dd75e8beb
Lantzman <lantzman@amazon.com>
2018-06-19 18:10:47 +0100

    fixup start up

--- kafka/1.1.0/scripts/start_server.sh
+++ kafka/1.1.0/scripts/start_server.sh
@@ -3,5 +3,6 @@ source `dirname "$0"`/common.sh
 
 echo Starting zookeeper daemon ...
 echo "$(cat $KAFKA_HOME/config/zookeeper-local.properties)"
+cd $KAFKA_HOME # Relative data file
 #su - root -c "nohup $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties"
 su - root -c "$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper-local.properties"
