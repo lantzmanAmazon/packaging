672c4f954a04589f8efaea9a98baf041faf879c4
Lantzman <lantzman@amazon.com>
2018-06-19 19:40:23 +0100

    typo

--- kafka/1.1.0/scripts/check_stat.sh
+++ kafka/1.1.0/scripts/check_stat.sh
@@ -4,7 +4,7 @@ source `dirname "$0"`/common.sh
 echo stat | nc localhost 2181
 SANITY=$(echo stat | nc localhost 2181 | grep "Node count:")
 
-if [ -z "$ALREADY_MOUNTED" ]; then # No good
+if [ -z "$SANITY" ]; then # Should be visible when started correctly
     echo Zookeeper failed to start !
     echo Config: "$(cat $KAFKA_HOME/config/zookeeper-local.properties)"
     echo Instance: "$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document)"
