0eb556cd5f2c0ecf96a6fdac0d4adcd9f7684f91
Lantzman <lantzman@amazon.com>
2018-06-21 14:02:34 +0100

    Script tweaks after running with Kafka

--- kafka/1.1.0/scripts/install_dependencies.sh
+++ kafka/1.1.0/scripts/install_dependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source `dirname "$0"`/common.sh
 
+echo Installing Java8
+su - root -c "yum install -y java-1.8.0-openjdk-src.x86_64"
+su - root -c "alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java"
+
 echo Extracting Kafka to $KAFKA_HOME ...
 su - root -c 'mkdir -p /app/kafka'
 su - root -c  "cp `dirname "$0"`/../$KafkaFile /app/$KafkaFile"
--- kafka/1.1.0/scripts/start_server.sh
+++ kafka/1.1.0/scripts/start_server.sh
@@ -4,4 +4,5 @@ source `dirname "$0"`/common.sh
 echo Starting zookeeper daemon ...
 echo "$(cat $KAFKA_HOME/config/zookeeper-local.properties)"
 cd $KAFKA_HOME # Relative data file
+# TODO: set KAFKA_HEAP_OPTS with the appropriate values, by default it's -Xmx512M -Xms512M
 su - root -c "$KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper-local.properties"
