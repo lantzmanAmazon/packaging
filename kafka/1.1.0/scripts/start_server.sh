55c438398311e9f6432d4e8ba73fd07fa5de5288
Lantzman <lantzman@amazon.com>
2018-06-25 14:05:15 +0100

    miniman memory (free instance)

--- kafka/1.1.0/scripts/start_server.sh
+++ kafka/1.1.0/scripts/start_server.sh
@@ -5,5 +5,5 @@ echo Starting zookeeper daemon ...
 echo "$(cat $KAFKA_HOME/config/zookeeper-local.properties)"
 cd $KAFKA_HOME # Relative data file
 # TODO: set KAFKA_HEAP_OPTS with the appropriate values, by default it's -Xmx512M -Xms512M
-export KAFKA_HEAP_OPTS="-Xmx256M -Xms256M"
+export KAFKA_HEAP_OPTS="-Xmx128M -Xms128M"
 su - root -c "$KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper-local.properties"
