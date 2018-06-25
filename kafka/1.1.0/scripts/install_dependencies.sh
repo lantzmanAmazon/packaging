3bc214dc611a029f95c01dccc55e87c0ddd389f3
Lantzman <lantzman@amazon.com>
2018-06-25 10:52:02 +0100

    reduces memory and cp jmterm

--- kafka/1.1.0/scripts/install_dependencies.sh
+++ kafka/1.1.0/scripts/install_dependencies.sh
@@ -9,3 +9,7 @@ echo Extracting Kafka to $KAFKA_HOME ...
 su - root -c 'mkdir -p /app/kafka'
 su - root -c  "cp `dirname "$0"`/../$KafkaFile /app/$KafkaFile"
 tar -zxvf /app/$KafkaFile -C /app/kafka
+
+
+# For debugging purposes only, copy JMLTerm for command line jmx tinkering
+aws s3 cp s3://eyal-kafka-monitor/221e15ec-9a28-4982-876e-b4fe7afb2cfd/kafka-monitor/jmx/jmxterm-1.0.0-uber.jar . 
