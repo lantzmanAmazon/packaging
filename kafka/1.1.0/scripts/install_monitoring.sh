f77196ccd41a443a0401d27e322a240f0fb41f74
Lantzman <lantzman@amazon.com>
2018-06-25 14:45:09 +0100

    fixup

--- kafka/1.1.0/scripts/install_monitoring.sh
+++ kafka/1.1.0/scripts/install_monitoring.sh
@@ -7,5 +7,5 @@ simpleversion=1.0
 monitoringRoot=/app/monitoring/$simpleversion
 su - root -c "mkdir -p $monitoringRoot"
 aws s3 cp --recursive s3://eyal-kafka-monitor/$buildversion/kafka-monitor $monitoringRoot
-su - root -c "cp $monitoringRoot/build/lib/* $monitoringRoot" #Flatten
+su - root -c "cp $monitoringRoot/build/libs/* $monitoringRoot" #Flatten
 su - root -c "chmod 777 $monitoringRoot/*" #Script execution
